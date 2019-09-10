244825af0f3e176e028f2cf6d86af5b333370af6
Joshua Jones <joshuajones@trentu.ca>
2019-09-10 10:32:23 +0700

    #100101

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './components/Routes';
+import axios from 'axios'
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
 
