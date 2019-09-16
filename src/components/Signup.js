f1440ae53a620eb30c7ff3331823a8e324fee435
Joshua Jones <joshuajones@trentu.ca>
2019-09-16 12:53:19 +0700

    changed signup redirect from '/' to '/places'

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -36,7 +36,7 @@ class Signup extends React.Component {
 		).then(res => {
 			console.log('data', res.data)
 			localStorage.setItem('token', res.data)
-			this.props.history.push("/")
+			this.props.history.push("/places")
 			}
 		)
 	} else {
