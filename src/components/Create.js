b906cb5c6e588e9fefa610e186baf4769143dbc2
Joshua Jones <joshuajones@trentu.ca>
2019-10-15 21:52:09 +0700

    added changeField method
    Need to figure out how to 'pop' amenity when checkbox is unclicked, perhaps class change?

--- src/components/Create.js
+++ src/components/Create.js
@@ -12,7 +12,10 @@ import axios from 'axios'
 class Create extends React.Component {
 
 	state={
-		user:{}
+		user:{},
+		place:{
+			amenities: []
+		}
 	}
 
 	componentWillMount() {
@@ -29,6 +32,23 @@ class Create extends React.Component {
 		}
 	}
 
+	postPlace = () => {
+
+	}
+
+	changeField = (e, field) => {
+		let place = this.state.place
+		place[field] = e.target.value
+		this.setState({place})
+		console.log(this.state.place);
+	}
+
+	addAmenities = (amenity) => {
+		let place = this.state.place
+		place.amenities.push(amenity)
+		this.setState({place})
+		console.log(this.state.place);
+	}
 
 
 
@@ -44,27 +64,27 @@ class Create extends React.Component {
 							<form>
 								<div className="group">
 									<label>Title</label>
-									<input type="text" />
+									<input onChange={(e)=>this.changeField(e,'title')} type="text" />
 								</div>
 								<div className="group">
 									<label>Description</label>
-									<textarea></textarea>
+									<textarea onChange={(e)=>this.changeField(e,'description')}></textarea>
 								</div>
 								<div className="group">
 									<label>City or Town</label>
-									<input type="text" />
+									<input onChange={(e)=>this.changeField(e,'city')} type="text" />
 								</div>
 								<div className="group">
 									<label>Country</label>
-									<input type="text" />
+									<input onChange={(e)=>this.changeField(e,'country')} type="text" />
 								</div>
 								<div className="group">
 									<label>Price per Night (USD)</label>
-									<input type="number" />
+									<input onChange={(e)=>this.changeField(e,'price')} type="number" />
 								</div>
 								<div className="group">
 									<label>Type</label>
-									<select>
+									<select onChange={(e)=>this.changeField(e,'type')}>
 										<option>Entire Villa</option>
 										<option>Entire House</option>
 										<option>Entire Apartment</option>
@@ -76,42 +96,42 @@ class Create extends React.Component {
 								</div>
 								<div className="group">
 									<label>Number of Rooms</label>
-									<input type="number" />
+									<input onChange={(e)=>this.changeField(e,'rooms')} type="number" />
 								</div>
 								<div className="group">
 									<label>Number of Bathrooms</label>
-									<input type="number" />
+									<input onChange={(e)=>this.changeField(e,'bathrooms')} type="number" />
 								</div>
 								<div className="group">
 									<label>Maximum number of Guests</label>
-									<input type="number" />
+									<input onChange={(e)=>this.changeField(e,'guests')} type="number" />
 								</div>
 								<div className="group">
 									<label>Upload Photos</label>
-									<input type="file" multiple />
+									<input onChange={(e)=>this.changeField(e,'images')} type="file" multiple />
 								</div>
 								<div className="group">
 									<label>Amenities</label>
 									<label className="checkbox">
-										<input type="checkbox" /> Swimming Pool
+										<input onChange={(e)=>this.addAmenities('Swimming Pool')} type="checkbox" /> Swimming Pool
 									</label>
 									<label className="checkbox">
-										<input type="checkbox" /> Kitchen
+										<input onChange={(e)=>this.addAmenities('Kitchen')} type="checkbox" /> Kitchen
 									</label>
 									<label className="checkbox">
-										<input type="checkbox" /> Wi-Fi
+										<input onChange={(e)=>this.addAmenities('Wi-Fi')} type="checkbox" /> Wi-Fi
 									</label>
 									<label className="checkbox">
-										<input type="checkbox" /> TV
+										<input onChange={(e)=>this.addAmenities('TV')} type="checkbox" /> TV
 									</label>
 									<label className="checkbox">
-										<input type="checkbox" /> Gym
+										<input onChange={(e)=>this.addAmenities('Gym')} type="checkbox" /> Gym
 									</label>
 									<label className="checkbox">
-										<input type="checkbox" /> Iron
+										<input onChange={(e)=>this.addAmenities('Iron')} type="checkbox" /> Iron
 									</label>
 									<label className="checkbox">
-										<input type="checkbox" /> Air Conditioning
+										<input onChange={(e)=>this.addAmenities('Air Conditioning')} type="checkbox" /> Air Conditioning
 									</label>
 								</div>
 								<button className="primary">Publish this Place</button>
